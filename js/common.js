2110cabc026b5835cbf0d4a30966625434cf146b
朱锡文 <574715866@qq.com>
2018-06-10 10:44:16 +0800

    d

---
+++ js/common.js
@@ -0,0 +1,6 @@
+// 获得URL上指定参的值
+function getQueryString(name) { 
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+  var r = window.location.search.substr(1).match(reg); 
+  if (r != null) return decodeURIComponent(r[2]); return null; 
+} 
